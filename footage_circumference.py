c9178e84da4eecf4b8b5f69c0d32d8fb458f0062
Jim Pov <jimpov@gmail.com>
2023-08-22 15:57:26 -0400

    removed superfluous comment from footage_circumference

--- footage_circumference.py
+++ footage_circumference.py
@@ -1,11 +1,3 @@
-'''
-1) Has a function to calculate the square footage of a house
-Reminder of Formula: Length X Width == Area
-
-2) Has a function to calculate the circumference of a circle
-
-Program in Jupyter Notebook should take in user input and use imported functions to calculate a circle's circumference or a houses square footage
-'''
 import math
 
 def house_area(length, width):
