3dd7bc926f833d74e28cf563effc27ea894518a9
Jim Pov <jimpov@gmail.com>
2023-08-29 11:11:44 -0400

    updated wednesday and thursday with clearer outputs

---
+++ foot_circum.py
@@ -0,0 +1,12 @@
+import math
+
+def house_area(length, width):
+    """calculate's a house's square footage if given length and width"""
+    area = length * width
+    return area 
+
+def circumference(radius):
+    """calculates a circle's circumference if given a radius"""
+    circumference = 2 * math.pi * radius
+    return circumference
+
